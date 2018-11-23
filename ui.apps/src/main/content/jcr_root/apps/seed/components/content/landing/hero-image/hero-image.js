fda09c0223b9344914ddac0d747ce3ea32c1069c
IND_Miguel Condori Mollehuara <xt1915@rimac.com.pe>
2018-11-22 19:35:33 -0500

    hero image updated

--- ui.apps/src/main/content/jcr_root/apps/seed/components/content/landing/hero-image/hero-image.js
+++ ui.apps/src/main/content/jcr_root/apps/seed/components/content/landing/hero-image/hero-image.js
@@ -2,25 +2,13 @@
 var global = this;
 
 use(function () {
-    var CONST = {
-        FULL_WIDTH: 'useFullWidth',
-        KEEP_RATIO: 'keepRatio'
-    };
-
-    var classList = 'we-HeroImage';
-
-    if (global.granite.resource.properties[CONST.FULL_WIDTH] == 'true') {
-        classList += ' width-full';
-    }
-    if (global.granite.resource.properties[CONST.KEEP_RATIO] == 'true') {
-        classList += ' ratio-16by9';
-    }
-
-    var renditionPath = resource.path + ".img.jpeg";
+   
+    var renditionPrimaryPath = resource.path + "/primary.img.jpeg";
+    var renditionSecondaryPath = resource.path + "/secondary.img.png";
     return {
-        classList: classList,
         image: {
-            src: renditionPath
+            src: renditionPrimaryPath,
+            srcSecondary: renditionSecondaryPath
         }
     };
 });
